cf3d8e1fd9330d830a885a3277f444c030379be3
karoline <karoline.wahl@hotmail.no>
2023-03-18 15:40:35 +0100

    New endpoint to get all unique envs from db.

--- routes/run_route.py
+++ routes/run_route.py
@@ -4,32 +4,17 @@ from fastapi.encoders import jsonable_encoder
 from starlette.responses import JSONResponse
 from models.run import BaseRun
 from typing import List
-import utils
-
-# TODO for tomorrow:
-#  research how the REST endpoints should look when using @router instead of @app
-#  figure out if the imports are correct or not
+from optuna_tuning.manager import collection
 
 # prefix: start of every decorator provided by fastapi in this particular page
 # tags: the functionality of this category
-router = APIRouter(prefix='/runs', tags=['crud/rest'])
+router = APIRouter(prefix='/api', tags=['crud/rest'])
 
-storage = utils.get_yaml_val("../config/db_config.yml", "storage")
-db = utils.get_yaml_val("../config/db_config.yml", "db")
-collection = utils.get_yaml_val("../config/db_config.yml", "collection")
-
-
-# TODO No need for this one
-@router.post("/", response_description="Add new run", status_code=status.HTTP_201_CREATED, response_model=BaseRun)
-async def create_run(request: Request, run: BaseRun):
-    # Decode JSON request body into Python dictionary before passing to client
-    run = jsonable_encoder(run)
-    new_run = await request.app.database['runs']['run'].insert_one(run)
-    # new_run = await db[collection].insert_one(run)
-    created_run = await request.app.database['runs']['run'].find_one({"_id": new_run.inserted_id})
-    # created_run = await db[collection].find_one({"_id": new_run.inserted_id})
-    # return JSONResponse(status_code=status.HTTP_201_CREATED, content=created_run)
-    return created_run
+@router.get("/", response_description="List of all unique envs in db",
+            response_model=List[BaseRun])  # TODO probably change response model entity
+async def list_envs():
+    envs = await collection.distinct('env')
+    return envs
 
 
 @router.get("/env", response_description="List the top trial for each algorithm for selected env",
@@ -57,19 +42,9 @@ async def list_runs_for_env_alg(env: str, alg: str, limit: int) -> List[BaseRun]
     return runs
 
 
-@router.get("/{id}", response_description="Get a single run")
+@router.get("/env/alg/{id}", response_description="Get a single run")
 async def show_run(id: str) -> BaseRun:
     if (run := await collection.find_one({"_id": id})) is not None:
         return run
 
     raise HTTPException(status_code=404, detail=f"Run with id {id} not found")
-
-
-@router.delete("/{id}", response_description="Delete a run")
-async def delete_student(id: str):
-    delete_result = await collection.delete_one({"_id": id})
-
-    if delete_result.deleted_count == 1:
-        return JSONResponse(status_code=status.HTTP_204_NO_CONTENT)
-
-    raise HTTPException(status_code=404, detail=f"Run with {id} not found")
