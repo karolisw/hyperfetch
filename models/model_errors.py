28dc6401d831a1aea5b17bc28c45ae7475e05c28
karoline <karoline.wahl@hotmail.no>
2023-04-05 10:04:27 +0200

    Errors for Pydantic-model CRUD operations.

---
+++ models/model_errors.py
@@ -0,0 +1,22 @@
+# # Installed # #
+from pydantic import BaseModel, Field
+
+__all__ = ("BaseError", "BaseIdentifiedError", "NotFoundError", "AlreadyExistsError")
+
+
+class BaseError(BaseModel):
+    message: str = Field(..., description="Error message or description")
+
+
+class BaseIdentifiedError(BaseError):
+    identifier: str = Field(..., description="Unique identifier which this error references to")
+
+
+class NotFoundError(BaseIdentifiedError):
+    """The entity does not exist"""
+    pass
+
+
+class AlreadyExistsError(BaseIdentifiedError):
+    """An entity being created already exists"""
+    pass
