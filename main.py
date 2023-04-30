c36804fa93a35bcbed0a947877f0742bb3444b1b
karoline <karoline.wahl@hotmail.no>
2023-04-30 13:19:25 +0200

    removed logging.

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from starlette.middleware.cors import CORSMiddleware
 from src.routes.run_route import router
 from src.config.auth_connection import ALLOWED_HOSTS
 from src.utils.db_utils import connect_to_motor, close_motor_connection
-import logging
 
 app = FastAPI()
 
@@ -17,9 +16,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-logging.info(("Allowed hosts: "))
-logging.info(ALLOWED_HOSTS)
 
 app.add_event_handler("startup", connect_to_motor)
 app.add_event_handler("shutdown", close_motor_connection)
