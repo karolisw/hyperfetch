1e69aa8481f523ca42900f1d48c5c5fd3d4e43f3
karoline <karoline.wahl@hotmail.no>
2023-05-05 22:34:36 +0200

    Env vars loaded from .env during testing.

--- src/config/auth_connection.py
+++ src/config/auth_connection.py
@@ -1,12 +1,11 @@
 import os
 
-# Uncomment to run tests
-ALLOWED_HOSTS = "https://hyperfetch.online"
-MONGODB_URL="mongodb+srv://admin:adminKaroline12@hyperfetch.zxjvuqd.mongodb.net/?retryWrites=true&w=majority"
-MONGO_DB="hyperfetch"
-MONGO_COLLECTION="runs"
+# Used in dev and for tests
+from dotenv import load_dotenv
+load_dotenv()
 
-'''ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(",")
+# Will find environment variables from Azure when in prod
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(",")
 MONGODB_URL = os.environ.get("MONGODB_URL")
 MONGO_DB = os.environ.get("MONGO_DB")
-MONGO_COLLECTION = os.environ.get("MONGO_COLLECTION")'''
+MONGO_COLLECTION = os.environ.get("MONGO_COLLECTION")
