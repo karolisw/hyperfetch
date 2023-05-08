f125b4fe37c42967b9b5e73f2bdca889f01aa655
karoline <karoline.wahl@hotmail.no>
2023-05-08 15:19:19 +0200

    Reformatting.

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 import os
 import uvicorn as uvicorn
-from fastapi import FastAPI, Request,Response
+from fastapi import FastAPI, Request, Response
 from starlette.middleware.base import BaseHTTPMiddleware
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.middleware.trustedhost import TrustedHostMiddleware
 from src.routes.run_route import router
 from src.config.auth_connection import ALLOWED_HOSTS
 from src.utils.db_utils import connect_to_motor, close_motor_connection
-
-
 
 app = FastAPI()
 
@@ -23,6 +21,7 @@ class CheckOriginMiddleware(BaseHTTPMiddleware):
         response.headers["Access-Control-Allow-Origin"] = origin
         response.headers["Access-Control-Allow-Headers"] = "Content-Type"
         return response
+
 
 app.add_middleware(
     CORSMiddleware,
@@ -39,12 +38,11 @@ app.add_middleware(
     CheckOriginMiddleware
 )
 
-
 app.add_event_handler("startup", connect_to_motor)
 app.add_event_handler("shutdown", close_motor_connection)
 
 app.include_router(router)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", 
+    uvicorn.run("main:app", host="0.0.0.0",
                 port=int(os.environ.get("PORT", 443)))
