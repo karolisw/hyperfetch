cdf101122d086f25410a660143d7e351a47e7681
karoline <karoline.wahl@hotmail.no>
2023-05-08 10:05:35 +0200

    Renamed.

--- src/routes/run_route.py
+++ src/routes/run_route.py
@@ -2,7 +2,7 @@
 from fastapi import Depends
 from starlette.status import HTTP_404_NOT_FOUND, HTTP_201_CREATED
 from src.config.mongodb import get_database
-from src.crud.run_crud import *
+from src.dal.dal_run import *
 from src.models.receive_run import EnvsRead, RunsRead
 from src.utils.exceptions import RunAlreadyExistsException, get_exception_responses, RunNotFoundException
 from typing import Any, Callable
@@ -34,7 +34,7 @@ class APIRouter(FastAPIRouter):
 
 
 # tags: the functionality of this category
-router = APIRouter(tags=['crud/rest'])
+router = APIRouter(tags=['dal/rest'])
 
 
 @router.get("/api/", response_description="List of all unique envs in db",
@@ -73,6 +73,7 @@ async def fetch_runs_for_env_alg(env: str, alg: str, limit: int, db: AsyncIOMoto
 async def fetch_run(run_id: str, db: AsyncIOMotorClient = Depends(get_database)) -> RunRead:
     run = await show_run(conn=db, run_id=run_id)
     return run
+
 
 # Not in use and must be commented out if the project should be made public.
 @router.delete("/api/delete/{run_id}", response_description="Delete a single run by its unique ID",
